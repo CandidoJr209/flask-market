6094bad4f46385b33702789ac9f750f68fcd08b7
Cândido <candido101209@gmail.com>
2021-05-23 08:35:24 -0300

    register page added

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import item
+from market.forms import RegisterForm
 
 @app.route("/")
 def home():
@@ -10,3 +11,9 @@ def home():
 def market_page():
     items = item.query
     return render_template('market.html', items=items)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
