2c39e114a6e93fd324257cc9b38071b9f8dc82cf
Cândido <candido101209@gmail.com>
2021-06-08 14:08:18 -0300

    bug login fixed

--- market/models.py
+++ market/models.py
@@ -1,9 +1,15 @@
 from sqlalchemy.orm import backref
 from werkzeug.exceptions import default_exceptions
-from market import db
+from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
 
      id = db.Column(db.Integer(), primary_key=True)
      username = db.Column(db.String(32), unique=True, nullable=False)
@@ -19,6 +25,9 @@ class User(db.Model):
      @password.setter
      def password(self, plain_text_password):
         self.password_hash = bcrypt.get_password_hash(plain_text_password).decode('utf-8')
+
+     def check_password_correction(self, attempted_password):
+         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 
 class item(db.Model):
